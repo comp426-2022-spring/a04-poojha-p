8d103825f97c25ae6b4674cec1820d3a38d61152
Poojha Palle <poojhap@ad.unc.edu>
2022-04-30 11:31:11 -0400

    create db if it doesn't exist

--- database.js
+++ database.js
@@ -9,4 +9,21 @@ let row = stmt.get();
 
 if (row === undefined) {
     console.log('Your database appears to be empty. I will initialize it now.');
+
+    const sqlInit = `
+            CREATE TABLE accesslog (
+                accesslogid INTEGER PRIMARY KEY, 
+                remoteaddr TEXT, 
+                remoteuser TEXT, 
+                time TEXT, 
+                method TEXT, 
+                url TEXT, 
+                protocol TEXT,
+                httpversion TEXT, 
+                status TEXT, 
+                referer TEXT,
+                useragent TEXT
+        );`
+    
+        db.exec(sqlInit);
 }
