48deb71fc6e5c2882dd0d1b73fe9e2f717c6a62d
Poojha Palle <poojhap@ad.unc.edu>
2022-04-30 11:30:10 -0400

    prepare db

--- database.js
+++ database.js
@@ -3,3 +3,6 @@
 const Database = require('better-sqlite3');
 
 const db = new Database('log.db');
+
+const stmt = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`);
+let row = stmt.get();
--- server.js
+++ server.js
@@ -16,67 +16,6 @@ const server = app.listen(port, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%', port))
 });
 
-if (args.log == 'false') {
-    console.log("NOTICE: not creating file access.log")
-} else {
-    const accessLog = fs.createWriteStream('access.log', { flags: 'a' })
-    app.use(morgan('combined', { stream: accessLog }))
-}
-
-app.get("/app/", (req, res, next) => {
-    res.json({"message":"Your API works! (200)"});
-	res.status(200);
-});
-
-const help = (`
-server.js [options]
---port	Set the port number for the server to listen on. Must be an integer
-            between 1 and 65535.
---debug	If set to true, creates endlpoints /app/log/access/ which returns
-            a JSON access log from the database and /app/error which throws 
-            an error with the message "Error test successful." Defaults to 
-            false.
---log		If set to false, no log files are written. Defaults to true.
-            Logs are always written to database.
---help	Return this message and exit.
-`)
-// If --help or -h, echo help text to STDOUT and exit
-if (args.help || args.h) {
-    console.log(help)
-    process.exit(0)
-}
-
-app.use( (req, res, next) => {
-    let logdata = {
-        remoteaddr: req.ip,
-        remoteuser: req.user,
-        time: Date.now(),
-        method: req.method,
-        url: req.url,
-        protocol: req.protocol,
-        httpversion: req.httpVersion,
-        status: res.statusCode,
-        referer: req.headers['referer'],
-        useragent: req.headers['user-agent']
-    }
-
-    console.log(logdata)
-    const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
-    const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
-    next();
-  });
-
-  if ((args.debug == true) || (args.d == true)) {
-    app.get('/app/log/access', (req, res, next) => {
-      const statement = db.prepare('SELECT * FROM accesslog').all();
-      res.status(200).json(statement);
-  });
-
-  app.get('/app/error', (req, res, next) => {
-    throw new Error('Successful: Error.')
-  })
-}
-
 // a03 part
 app.get('/app/', (req, res) => {
     res.statusCode = 200;
