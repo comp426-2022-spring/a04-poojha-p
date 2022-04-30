1807b3077c9be53cb833145a81c4ead28ef91702
Poojha Palle <poojhap@ad.unc.edu>
2022-04-30 11:30:46 -0400

    create db if it doesn't exist

--- database.js
+++ database.js
@@ -6,3 +6,7 @@ const db = new Database('log.db');
 
 const stmt = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`);
 let row = stmt.get();
+
+if (row === undefined) {
+    console.log('Your database appears to be empty. I will initialize it now.');
+}
