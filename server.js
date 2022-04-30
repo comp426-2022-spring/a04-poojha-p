eb359217dda140daa95d803a667a0a8b41476a2f
Poojha Palle <poojhap@ad.unc.edu>
2022-04-30 11:35:17 -0400

    insert into db p1

--- server.js
+++ server.js
@@ -54,6 +54,11 @@ app.use( (req, res, next) => {
         referer: req.headers['referer'],
         useragent: req.headers['user-agent']
     }
+
+    console.log(logdata)
+    const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
+    const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
+    next();
   });
 
 // a03 part
